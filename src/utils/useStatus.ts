365f138a24f842ccc59a2d71a33c05a881c5cf06
Kyle <sully8437@gmail.com>
2024-02-20 15:50:32 -0600

    check useStatus in

---
+++ src/utils/useStatus.ts
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export type StatusType =
+  | 'inactive'
+  | 'success'
+  | 'error'
+  | 'label'
+  | 'notification';
+
+export interface Status {
+  type: StatusType;
+  message: string | React.ReactNode;
+}
+
+const useStatus = (
+  initialStatus: Status = { type: 'inactive', message: '' }
+) => {
+  const [status, setStatus] = useState<Status>(initialStatus);
+
+  const updateStatus = (
+    type: StatusType,
+    message: string | React.ReactNode = ''
+  ) => {
+    setStatus({ type, message });
+  };
+
+  return { status, updateStatus };
+};
+
+export default useStatus;
