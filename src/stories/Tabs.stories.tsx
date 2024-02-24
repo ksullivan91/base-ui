1434988581c95e191b1e648ea132cdf6baae9fb2
Kyle <sully8437@gmail.com>
2024-02-24 10:23:59 -0600

    fix tab import in tab story

--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta } from '@storybook/react';
 import styled from 'styled-components';
-import { Tab, Tabs, TabsList } from '../components/Tabs';
+import Tabs, { Tab, TabsList } from '../components/Tabs';
 import * as React from 'react';
 
 const Container = styled.div`
